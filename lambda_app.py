72702eeda94fe3bb33830ebd77ac3fe62a6e2c15
Derek Morrison <me@derek-morrison.com>
2022-12-22 16:37:53 -0600

    Switch to using POST

--- lambda_app.py
+++ lambda_app.py
@@ -8,17 +8,20 @@ from io import BytesIO
 import base64
 
 def handler(event, context):
-    print("event.queryStringParameters: ", event["queryStringParameters"])
-    calculator = RefiVsInvestCalculator(event["queryStringParameters"])
+    print("event: ", event)
+    print('POST body:', event['body'])
+
+    params = event['body']
+    calculator = RefiVsInvestCalculator(params)
     calculator.calculate_balances_over_time()
 
     return {
         'statusCode': 200,
         'body': {
-            'myMessage': 'Try generating the image again.',
-            'endingLiquidBalanceOverTime': calculator.ending_liquid_balance_over_time,
+            # 'msgFromHandler': 'Trying to send POST params.',
             # 'event': event,
-            'balancePlotBase64Img': calculator.get_balances_plot_base64()
+            'endingLiquidBalanceOverTime': calculator.ending_liquid_balance_over_time,
+            # 'balancePlotBase64Img': calculator.get_balances_plot_base64()
         },
     }
 
